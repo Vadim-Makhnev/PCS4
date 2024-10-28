11a549523daa4597870e37e8bcdf4457f5b2743a
Vadim Makhnev <vadik.mahnev@yandex.ru>
2024-10-28 14:29:56 +0300

    Add existing project files to Git

--- lib/main.dart
+++ lib/main.dart
@@ -11,27 +11,20 @@ class MyApp extends StatelessWidget {
       title: "Game Store",
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          title: const Center(
-            child: Text(
-              "Game Store",
-              style: TextStyle(
-                fontSize: 40,
-                color: Colors.white70,
-              ),
-            ),
-          ),
-        ),
-        body:  GameList(),
-      ),
+      home: const GameListScreen(),
     );
   }
 }
 
-class GameList extends StatelessWidget {
-  final List<Map<String, String>> games = [
+class GameListScreen extends StatefulWidget {
+  const GameListScreen({super.key});
+
+  @override
+  _GameListScreenState createState() => _GameListScreenState();
+}
+
+class _GameListScreenState extends State<GameListScreen> {
+  List<Map<String, String>> games = [
     {
       'name': 'GTA VI',
       'image': 'https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1jAyV3.img',
@@ -48,26 +41,64 @@ class GameList extends StatelessWidget {
     {
       'name': 'Cyberpunk 2077',
       'image': 'https://i0.wp.com/mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/81a4e680815973.5cec6bcf6aa1a.jpg?ssl=1',
-      'description': 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.'
-    },
-    {
-      'name': 'Detroit: Become Human',
-      'image': 'https://pic.rutubelist.ru/video/bc/aa/bcaa4fa6410b7dc5b3ce156ed6f29e46.jpg',
-      'description': 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.'
+      'description': 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.'
     },
     {
       'name': 'The Last Of Us',
       'image': 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/110a6323982149.5632c1abead45.jpg',
       'description': 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.'
-    },
-    {
-      'name': 'Beyond Two Souls',
-      'image': 'https://pic.rutubelist.ru/video/32/37/32372abf9e74c1aaf4b11e241728456b.jpg',
-      'description': 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.'
-    },
+    }
   ];
 
-  GameList({super.key});
+  void addGame(Map<String, String> newGame) {
+    setState(() {
+      games.add(newGame);
+    });
+  }
+
+  void deleteGame(int index) {
+    setState(() {
+      games.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+            "Game Store",
+            style: TextStyle(
+              fontSize: 40,
+              color: Colors.white70,
+            ),
+          ),
+        ),
+        backgroundColor: Colors.black,
+      ),
+      body: GameList(games: games, onDeleteGame: deleteGame),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.white70,
+        child: const Icon(Icons.add, color: Colors.black),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => AddGameScreen(onAddGame: addGame),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class GameList extends StatelessWidget {
+  final List<Map<String, String>> games;
+  final Function(int) onDeleteGame;
+
+  const GameList({super.key, required this.games, required this.onDeleteGame});
 
   @override
   Widget build(BuildContext context) {
@@ -84,8 +115,8 @@ class GameList extends StatelessWidget {
               Expanded(
                 child: Text(
                   games[index]['description']!,
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
                   style: const TextStyle(color: Colors.white70),
                 ),
               ),
@@ -99,6 +130,12 @@ class GameList extends StatelessWidget {
                 ),
               ),
             ],
+          ),
+          trailing: IconButton(
+            icon: const Icon(Icons.delete, color: Colors.red),
+            onPressed: () {
+              onDeleteGame(index);
+            },
           ),
           onTap: () {
             Navigator.push(
@@ -126,7 +163,8 @@ class GameDetail extends StatelessWidget {
   final String imageUrl;
   final String description;
 
-  const GameDetail({super.key, 
+  const GameDetail({
+    super.key,
     required this.name,
     required this.imageUrl,
     required this.description,
@@ -167,3 +205,91 @@ class GameDetail extends StatelessWidget {
     );
   }
 }
+
+class AddGameScreen extends StatefulWidget {
+  final Function(Map<String, String>) onAddGame;
+
+  const AddGameScreen({super.key, required this.onAddGame});
+
+  @override
+  _AddGameScreenState createState() => _AddGameScreenState();
+}
+
+class _AddGameScreenState extends State<AddGameScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String _name = '';
+  String _imageUrl = '';
+  String _description = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Add New Game"),
+        backgroundColor: Colors.black,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Game Name'),
+                onSaved: (value) {
+                  _name = value ?? '';
+                },
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter the game name';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Image URL'),
+                onSaved: (value) {
+                  _imageUrl = value ?? '';
+                },
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter the image URL';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Description'),
+                onSaved: (value) {
+                  _description = value ?? '';
+                },
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a description';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+                    widget.onAddGame({
+                      'name': _name,
+                      'image': _imageUrl,
+                      'description': _description,
+                    });
+                    Navigator.pop(context);
+                  }
+                },
+                child: const Text('Add Game'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
